04621980914f6f466f70c22395cb6be08dab4e8e
RulerOfCakes <fragmentbombs@gmail.com>
2023-02-12 23:17:58 +0900

    fix: proper link components

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,19 +1,43 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 
 const Header: NextPage = () => {
   return (
     <div className="box-border flex w-[120rem] flex-row items-start justify-start gap-[0.63rem] overflow-hidden p-[0.63rem] text-left font-noto-sans-kr text-[1.25rem] text-black">
       <div className="flex flex-row items-center justify-start gap-[3.13rem] overflow-hidden py-[0.63rem] px-[1.88rem]">
         <button className="relative h-[4.38rem] w-[21rem] shrink-0 cursor-pointer bg-[transparent] bg-[url(../../public/logo.png)] bg-cover bg-[top] bg-no-repeat p-0 [border:none]" />
-        <a className="relative text-[inherit] [text-decoration:none]">
+        <Link
+          className="relative text-[inherit] [text-decoration:none]"
+          href="products"
+        >
           인테리어 상품
-        </a>
-        <div className="relative">커스텀 PBV</div>
-        <div className="relative">중고 거래</div>
+        </Link>
+        <Link
+          className="relative text-[inherit] [text-decoration:none]"
+          href="pbv"
+        >
+          커스텀 PBV
+        </Link>
+        <Link
+          className="relative text-[inherit] [text-decoration:none]"
+          href=""
+        >
+          중고 거래
+        </Link>
       </div>
       <div className="flex flex-1 flex-row items-center justify-end gap-[3.13rem] self-stretch overflow-hidden py-[0.63rem] px-[1.88rem]">
-        <div className="relative">회원가입</div>
-        <div className="relative">로그인</div>
+        <Link
+          className="relative text-[inherit] [text-decoration:none]"
+          href="auth/signup"
+        >
+          회원가입
+        </Link>
+        <Link
+          className="relative text-[inherit] [text-decoration:none]"
+          href="auth/login"
+        >
+          로그인
+        </Link>
       </div>
     </div>
   );
