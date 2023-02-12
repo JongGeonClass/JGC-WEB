c2ba66cb064ef9783d533982125d62cb38760d6b
RulerOfCakes <fragmentbombs@gmail.com>
2023-02-12 19:20:43 +0900

    feat: added tailwindcss

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,1 +1,16 @@
-// main test page containing links to all other test directory pages in next.js
+import React from "react";
+import Link from "next/link";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="font-bold">Welcome to the Application!</h1>
+      <p>Please log in or sign up to continue.</p>
+      <Link href="test/auth/signup">Sign Up</Link>
+      <br />
+      <Link href="test/auth/login">Log In</Link>
+    </div>
+  );
+};
+
+export default LandingPage;
