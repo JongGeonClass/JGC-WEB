7b6c2d36fa1803363d7d2a7e865ed09f7f832a89
RulerOfCakes <fragmentbombs@gmail.com>
2023-02-19 16:20:32 +0900

    fix: unsupported page type

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,10 @@
-import type { NextPage } from "next";
 import Header from "@/components/Header";
-import CartContainer from "./CartContainer";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 import Background from "public/background-temp.png";
-import { QueryClientProvider } from "@tanstack/react-query";
 import Main from "./main";
 
-const JGCCart: NextPage = () => {
+const JGCCart = () => {
   return (
     <div className="relative bg-white w-full h-[67.5rem] overflow-hidden flex flex-col items-start justify-start">
       <Header />
