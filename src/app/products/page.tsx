f8518727188cb982cab78a50f8afa90c2391875b
RulerOfCakes <fragmentbombs@gmail.com>
2023-02-21 01:12:49 +0900

    fix: type errors

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,10 @@
-import type { NextPage } from "next";
 import background from "public/background-temp.png";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Image from "next/image";
-import ProductListContainer from "@/components/ProductListContainer";
 import Main from "./main";
 
-const JGCProduct: NextPage = () => {
+const JGCProduct = () => {
   return (
     <div className="relative bg-white w-full overflow-hidden flex flex-col items-start justify-start text-left text-[1.25rem] text-black font-noto-sans-kr">
       <Header />
