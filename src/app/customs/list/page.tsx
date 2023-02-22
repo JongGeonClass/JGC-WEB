340cb3ff1f081270b6cdf70ac758e9132ef0cb75
최철민 <effect2110@hanyang.ac.kr>
2023-02-22 16:24:01 +0900

    add my page pbv feature.

---
+++ src/app/customs/list/page.tsx
@@ -0,0 +1,26 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+
+import Background from "public/background-temp.png";
+import Main from "./main";
+
+const JGCMyPageCustomPBV = () => {
+  return (
+    <div className="relative bg-white w-full h-[67.5rem] overflow-hidden flex flex-col items-start justify-start">
+      <Header />
+      <div className="self-stretch flex-1 relative bg-gainsboro-100 overflow-hidden">
+        <Image
+          className="absolute top-[0rem] left-[0rem] w-[120rem] h-[56.88rem] object-cover"
+          alt=""
+          src={Background}
+        />
+
+        <Main />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default JGCMyPageCustomPBV;
--- src/components/CarOptionsContainer.tsx
+++ src/components/CarOptionsContainer.tsx
@@ -3,20 +3,10 @@ import type { NextPage } from "next";
 const CarOptionsContainer: NextPage = () => {
   return (
     <div className="self-stretch overflow-hidden flex flex-col p-[0.63rem] items-center justify-start gap-[0.63rem] text-left text-[1.13rem] text-black font-noto-sans-kr">
-
-      <div className="overflow-hidden flex flex-row items-start justify-start gap-[0.63rem]">
-        <div className="self-stretch overflow-hidden flex flex-col p-[0.63rem] items-center justify-center">
-          <div className="relative font-light">커스텀 차량 이름 :</div>
-        </div>
-
-        <div className="self-stretch bg-whitesmoke overflow-hidden flex flex-col p-[0.63rem] items-center justify-center text-gray-400">
-          <div className="relative font-light">차량 이름을 입력하세요..</div>
-        </div>
-      </div>
-
       <div className="overflow-hidden flex flex-row p-[0.63rem] items-start justify-center gap-[0.63rem] text-[1.25rem]">
+        
         <div className="rounded-lg bg-white shadow-[0px_4px_10px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-col p-[0.63rem] items-start justify-start">
-          <div className="w-[26.75rem] overflow-hidden flex flex-row p-[0.63rem] box-border items-start justify-start text-[1.38rem] text-tomato">
+          <div className="w-[26.75rem] overflow-hidden flex flex-row p-[0.63rem] box-border items-start justify-start text-[1.38rem] text-red-500">
             <div className="flex-1 overflow-hidden flex flex-col p-[0.63rem] items-start justify-start">
               <b className="relative">공통옵션</b>
             </div>
@@ -47,8 +37,8 @@ const CarOptionsContainer: NextPage = () => {
           </div>
         </div>
         
-        <div className="rounded-lg bg-snow shadow-[0px_4px_10px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-col p-[0.63rem] items-start justify-start">
-          <div className="w-[26.75rem] overflow-hidden flex flex-row p-[0.63rem] box-border items-start justify-start text-[1.38rem] text-tomato">
+        <div className="rounded-lg bg-white shadow-[0px_4px_10px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-col p-[0.63rem] items-start justify-start">
+          <div className="w-[26.75rem] overflow-hidden flex flex-row p-[0.63rem] box-border items-start justify-start text-[1.38rem] text-red-500">
             <div className="flex-1 overflow-hidden flex flex-col p-[0.63rem] items-start justify-start">
               <b className="relative">캠핑용 차량 옵션</b>
             </div>
@@ -119,11 +109,6 @@ const CarOptionsContainer: NextPage = () => {
           </div>
         </div>
       </div>
-
-      <div className="rounded-base bg-white shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] overflow-hidden flex flex-col py-[0.94rem] rounded-md px-[5rem] items-start justify-start text-[1.5rem] text-red-500 border-r-[0.5px] border-solid border-tomato border-b-[0.8px] border-l-[0.5px] border-t-0">
-        <div className="relative font-medium text-red-500">커스텀 저장하기</div>
-      </div>
-      
     </div>
   );
 };
