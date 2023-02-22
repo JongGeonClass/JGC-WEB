a0783bbe6c82bb316a26c2e8d3f98d7e7527c003
thak1411 <thak1411@gmail.com>
2023-02-22 21:47:44 +0900

    Fix: deploy shell script for killing prev version process

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/ubuntu/jgc_web && sudo pnpm install && sudo pnpm run build && sudo pm2 start pnpm --name "jgc-web" -- start
+cd /home/ubuntu/jgc_web && sudo pnpm install && sudo pnpm run build && sudo pm2 kill && sudo pm2 start pnpm --name "jgc-web" -- start
