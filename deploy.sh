03546720afcdacfca2dbfeb143eea00285b95b03
thak1411 <thak1411@gmail.com>
2023-02-22 13:58:03 +0900

    Update: Pm2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/ubuntu/jgc_web && sudo pnpm install && sudo pnpm run build && sudo pnpm run start
+cd /home/ubuntu/jgc_web && sudo pnpm install && sudo pnpm run build && sudo pm2 start pnpm --name "jgc-web" -- start
